e17e4bce5f61662e2cd674b7ff41b5b95d1f6a69
LTOLVE01\olve <olve@bizzkit.com>
2023-05-23 19:24:11 +0900

    adding post, put and eval to elastic

--- elasticsearch/api.py
+++ elasticsearch/api.py
@@ -6,7 +6,7 @@ import urlpath
 from api._common.api import Api
 from api.elasticsearch.config import ElasticsearchConfiguration
 from api._common.response import Response
-from api.headers.header import Headers, accept_json, basic_authorization
+from api.headers.header import Headers, accept_json, basic_authorization, content_type_json
 
 class ElasticResponse(Response):
     config: ElasticsearchConfiguration | None = None
@@ -19,7 +19,7 @@ class ElasticResponse(Response):
 
 class Elasticsearch(Api):
     def __init__(self, config: ElasticsearchConfiguration, verify: bool = False):
-        super().__init__(config.base_url, verify, accept_json())
+        super().__init__(config.base_url, verify, content_type_json() + accept_json())
         self.config = config
     
     def _get_response(self, url: urlpath.URL, request: Callable):
@@ -54,18 +54,60 @@ class Elasticsearch(Api):
             params: Any | None = None, 
             headers: dict[str, str] | Headers | None = None) -> ElasticResponse:
         
-        url = self.base_url / path
+        url: urlpath.URL = self.base_url / path
 
         headers = self._get_headers(headers).to_dict()
 
         request = lambda: url.get(params=params, verify=self.verify, headers=headers)
         return self._get_response(url, request)
     
-    def post(self, path: str, data, params: Any | None = None, headers: dict[str, str] | Headers | None = None) -> Response:
-        return super().post(path, data, params, headers)
+    def post(self, 
+             path: str, 
+             data, 
+             params: Any | None = None, 
+             headers: dict[str, str] | Headers | None = None) -> ElasticResponse:
+        
+        url: urlpath.URL = self.base_url / path
+
+        headers = self._get_headers(headers).to_dict()
+
+        request = lambda: url.post(data=data, params=params, verify=self.verify, headers=headers)
+        return self._get_response(url, request)
     
-    def put(self, path: str, data, params: Any | None = None, headers: dict[str, str] | Headers | None = None) -> Response:
-        return super().put(path, data, params, headers)
+    def put(self, path: str, data, params: Any | None = None, headers: dict[str, str] | Headers | None = None) -> ElasticResponse:
+        
+        url: urlpath.URL = self.base_url / path
+
+        headers = self._get_headers(headers).to_dict()
+
+        request = lambda: url.put(data=data, params=params, verify=self.verify, headers=headers)
+        return self._get_response(url, request)
     
-    def delete(self, path: str, params: Any | None = None, headers: dict[str, str] | Headers | None = None) -> Response:
+    def delete(self, path: str, params: Any | None = None, headers: dict[str, str] | Headers | None = None) -> ElasticResponse:
         return super().delete(path, params, headers)
+
+    def _breakdown_request(self, request: str) -> tuple[str, str]:
+        method, *uri = request.split(' ')
+        method = method.lower()
+        uri = ' '.join(uri)
+        return method, uri
+
+    def eval(self, request: str, body: dict | None = None, print_response: bool = True):
+        method_name, uri = self._breakdown_request(request)
+
+        method = {'post': self.post, 'get': self.get, 'put': self.put, 'delete': self.delete}[method_name]
+        uri = urlpath.URL(uri)
+
+        if method_name in ('post', 'put'):
+            if body:
+                body = json.dumps(body)
+
+            response: Response = method(uri.path, body, params=uri.form_fields)
+
+        else:
+            response: Response = method(uri.path, params=uri.form_fields)
+
+        if print_response:
+            print(json.dumps(response.json, indent=4))
+
+        return response
